111b7fd1f4f47ae929a12f05720fee100b8de393
Aayush Shukla <oreus.aks@gmail.com>
2023-12-21 21:00:00 +0530

    Created the prime number function

---
+++ 3. Functions/d. prime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int num) {
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    
+    int number;
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << "The number " << number << " is prime.";
+    } else {
+        cout << "The number " << number << " is not prime";
+    }
+
+    return 0;
+}
